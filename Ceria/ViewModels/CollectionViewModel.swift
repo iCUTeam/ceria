6afb36d04198418425d31108b4aa6cba11f4978a
Kathleen Febiola <kathy.febiola02@gmail.com>
2022-10-17 17:46:00 +0700

    Make Collection ViewModel

--- Ceria/ViewModels/CollectionViewModel.swift
+++ Ceria/ViewModels/CollectionViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+final class CollectionViewModel {
+    
+    var collectibleItem: ObservableObject<String> = ObservableObject(value: "")
+    var collectibleName: ObservableObject<String> = ObservableObject(value: "")
+    var collectibleDesc: ObservableObject<String> = ObservableObject(value: "")
+    var collectibleOrigin: ObservableObject<String> = ObservableObject(value: "")
+    var isObtained: ObservableObject<Bool> = ObservableObject(value: false)
+    
+    var collectionArray: [Collection] = []
+    var feeder = CollectionFeeder()
+    
+    var obtainedStatus: [Bool] = []
+    
+    let defaults = UserDefaults.standard
+    
+    func getCollection() {
+        
+        let collectionArray = feeder.feedCollection()
+        
+        for collectibles in collectionArray
+        {
+            obtainedStatus.append(collectibles.isObtained)
+            defaults.set(obtainedStatus, forKey: "collectiblesObtainedStatus")
+        }
+        
+    }
+    
+    func obtainItem(index: Int)
+    {
+        obtainedStatus[index].toggle()
+        defaults.set(obtainedStatus, forKey: "collectiblesObtainedStatus")
+    }
+       
+}
