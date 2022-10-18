78437575d9496baf715731fe48b09fbbf32ca829
Kathleen Febiola <kathy.febiola02@gmail.com>
2022-10-18 11:40:11 +0700

    Make XIB View

--- Ceria/Views/Controllers/CollectionViewController.swift
+++ Ceria/Views/Controllers/CollectionViewController.swift
@@ -2,7 +2,7 @@
 //  CollectionViewController.swift
 //  Ceria
 //
-//  Created by Kevin Gosalim on 12/10/22.
+//  Created by Kathleen Febiola on 12/10/22.
 //
 
 import UIKit
@@ -16,10 +16,15 @@ class CollectionViewController: UIViewController, Storyboarded {
     {
         let scnView = SCNView()
         let scene = SCNScene(named: "Models.scnassets/CollectionScene.scn")
+        scnView.allowsCameraControl = false
         scnView.scene = scene
         
         return scnView
     }()
+    
+    var tarumpahLocked: SCNNode!
+    
+    var tarumpahUnlocked: SCNNode!
     
     private lazy var homeButton: MakeButton = {
         let button = MakeButton(image: "home.png", size: CGSize(width: 100, height: 100))
@@ -33,12 +38,32 @@ class CollectionViewController: UIViewController, Storyboarded {
         view.addSubview(collectionSceneView)
         view.addSubview(homeButton)
         setUpAutoLayout()
+        setupNode()
+        
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.numberOfTouchesRequired = 1
+        tapRecognizer.numberOfTapsRequired = 1
+        
+        tapRecognizer.addTarget(self, action: #selector(tapObject(recognizer:)))
+        
+        collectionSceneView.addGestureRecognizer(tapRecognizer)
         //Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         collectionSceneView.frame = view.bounds
+    }
+    
+    @objc func tapObject(recognizer: UITapGestureRecognizer)
+    {
+        
+    }
+    
+    func setupNode()
+    {
+        tarumpahLocked = collectionSceneView.scene?.rootNode.childNode(withName: "tarumpah_locked", recursively: true)
+        tarumpahUnlocked = collectionSceneView.scene?.rootNode.childNode(withName: "tarumpah_unlocked", recursively: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
