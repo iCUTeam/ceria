a6d25314c991e0b3092570a51db83e4257fb442c
kego <42563702+kegoaps@users.noreply.github.com>
2022-11-07 18:02:40 +0700

    fix cocoapods

---
+++ Pods/Target Support Files/Pods-Carita/Pods-Carita-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carita : NSObject
+@end
+@implementation PodsDummy_Pods_Carita
+@end
--- Pods/Target Support Files/Pods-Carita/Pods-Carita-umbrella.h
+++ Pods/Target Support Files/Pods-Carita/Pods-Carita-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CeriaVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CeriaVersionString[];
+FOUNDATION_EXPORT double Pods_CaritaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CaritaVersionString[];
 
